518ae1837460463a6c406606da85baebfecddafb
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-05-01 21:28:57 +0300

    Purchase List Added

--- src/components/PurchaseList.js
+++ src/components/PurchaseList.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
 
 export default class PurchaseList extends Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-
+            <View style={styles.container}>
                 <FlatList
-                    data={[{key: 'Beer'}, {key: 'Milk'}, {key: 'Bread'}, {key: 'Other'}]}
-                    renderItem={({item}) => <Text>{item.key}</Text>}
+                    data={[
+                        {key: 'Beer'},
+                        {key: 'Milk'},
+                        {key: 'Apples'},
+                        {key: 'Potatoes'},
+                        {key: 'Cat\'s Food'},
+                        {key: 'Meat'},
+                        {key: 'Bred'},
+                        {key: 'Cigarettes'},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
                 />
-
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 5,
+        paddingTop: 0
+    },
+    item: {
+        padding: 10,
+        fontSize: 20,
+        height: 44,
+    },
+})
+
+AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
